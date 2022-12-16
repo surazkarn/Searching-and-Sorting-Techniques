c065d73315ccd9c56d09d3ab3af780dba1f4a9bb
surazkarn <surazkarn8848@gmail.com>
2022-12-16 12:09:39 +0530

    bubble sort techniques

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,3 +1,26 @@
+
 public class BubbleSort {
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 7, 4, 1, 5, 3 };
+        int n = arr.length;
+
+        // bubble sort
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 
 }
